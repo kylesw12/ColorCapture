b004bcb8d8cd4ed4be895a845b31e91f37ba5308
Kyle <Kyle@MacBook-Pro.local>
2025-12-30 23:30:40 -0800

    Create a mask for yellow objects in camera Added function to util file which gets the lower and upper limit of a color in HSV space. Used this function on top of the frames captured by the webcam to create a mask for yellow objects.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import cv2
+from util import get_limits
 
+yellow = [0, 255, 255] # Yellow in BGR
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
 
-    cv2.imshow('frame', frame)
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower, upper = get_limits(color=yellow)
+
+    mask = cv2.inRange(hsvImage, lower, upper) # Create a mask for yellow objects
+
+    cv2.imshow('frame', mask)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
